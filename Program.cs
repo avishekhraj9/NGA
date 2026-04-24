35fd0a83cc72cc5d3987e86f88d93d1dd52945ef
AVISHEKH RAJ <75256697+avishekhraj9@users.noreply.github.com>
2026-04-24 09:36:28 +0530

    Day 4 Assignment Music Playlist Manager
    Scenario 4

--- Program.cs
+++ Program.cs
@@ -1,130 +1,109 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+
+// music playlist manager
+using System;
 using System.Collections.Generic;
+class MusicPlaylistManager
+{
+    LinkedList<string> playlist = new LinkedList<string>();
+
+    SortedList<int, string> songRatings = new SortedList<int, string>();
+
+    SortedDictionary<string, string> songGenres = new SortedDictionary<string, string>();
+
+    public void Addsong(string song, int rating, string artist)
+    {
+        playlist.AddLast(song);
+        songRatings[rating] = song;
+        songGenres[song] = artist;
+        Console.WriteLine($"Song'{song}' added");
+
+    }
+
+    public void RemoveSong(string song)
+    {
+        playlist.Remove(song);
+
+        int KeyToRemove = -1;
+        foreach (var kvp in songRatings)
+        {
+            if (kvp.Value == song)
+            {
+                KeyToRemove = kvp.Key;
+                break;
+            }
+        }
+        if (KeyToRemove != -1)
+        {
+            songRatings.Remove(KeyToRemove);
+        }
+        //remore from artist
+        string artistToRemove = null;
+        foreach (var kvp in songGenres)
+        {
+            if (kvp.Value == song)
+            {
+                artistToRemove = kvp.Key;
+                break;
+            }
+        }
+
+        if (artistToRemove != null)
+        {
+            songGenres.Remove(artistToRemove);
+        }
+            Console.WriteLine($"Song '{song}' removed");
+     }
+
+    //Display Playlist
+    public void DisplayPlaylist()
+    {
+        Console.WriteLine("Playlist:");
+        foreach (var song in playlist)
+        {
+            Console.WriteLine(song);
+        }
+    }
+
+    //Display songs by rating
+    public void DisplaySongsByRating()
+    {
+        Console.WriteLine("Songs by Rating:");
+        foreach (var kvp in songRatings)
+        {
+            Console.WriteLine($"Rating: {kvp.Key}, Song: {kvp.Value}");
+        }
+    }
+
+    //Display songs by genre
+    public void DisplaySongsByGenre()
+    {
+        Console.WriteLine("Songs by Genre:");
+        foreach (var kvp in songGenres)
+        {
+            Console.WriteLine($"Artist: {kvp.Key}, Song: {kvp.Value}");
+        }
+
+
+
+    }
+}
+
 
 class Program
 {
-    static List<string> posts = new List<string>();
-    static Dictionary<string, int> likes = new Dictionary<string, int>();
-    static HashSet<int> users = new HashSet<int>();
-    static Stack<string> actions = new Stack<string>();
-    static Queue<string> notifications = new Queue<string>();
-
-    static void Main()
+    static void Main(string[] args)
     {
-        while (true)
-        {
-            Console.WriteLine("\n===== Social Media System =====");
-            Console.WriteLine("1. Add User");
-            Console.WriteLine("2. Create Post");
-            Console.WriteLine("3. Like Post");
-            Console.WriteLine("4. Undo Last Action");
-            Console.WriteLine("5. Show Posts & Likes");
-            Console.WriteLine("6. Process Notifications");
-            Console.WriteLine("7. Exit");
-
-            Console.Write("Enter choice: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:
-                    AddUser();
-                    break;
-                case 2:
-                    CreatePost();
-                    break;
-                case 3:
-                    LikePost();
-                    break;
-                case 4:
-                    Undo();
-                    break;
-                case 5:
-                    ShowPosts();
-                    break;
-                case 6:
-                    ProcessNotifications();
-                    break;
-                case 7:
-                    return;
-                default:
-                    Console.WriteLine("Invalid choice!");
-                    break;
-            }
-        }
-    }
-
-    static void AddUser()
-    {
-        Console.Write("Enter User ID: ");
-        int id = Convert.ToInt32(Console.ReadLine());
-
-        if (users.Add(id))
-            Console.WriteLine("User added successfully!");
-        else
-            Console.WriteLine("User already exists!");
-    }
-
-    static void CreatePost()
-    {
-        Console.Write("Enter post content: ");
-        string post = Console.ReadLine();
-
-        posts.Add(post);
-        likes[post] = 0;
-
-        Console.WriteLine("Post created!");
-    }
-
-    static void LikePost()
-    {
-        Console.Write("Enter post to like: ");
-        string post = Console.ReadLine();
-
-        if (likes.ContainsKey(post))
-        {
-            likes[post]++;
-            actions.Push(post);
-            notifications.Enqueue(post + " got a like");
-
-            Console.WriteLine("Post liked!");
-        }
-        else
-        {
-            Console.WriteLine("Post not found!");
-        }
-    }
-
-    static void Undo()
-    {
-        if (actions.Count > 0)
-        {
-            string lastPost = actions.Pop();
-            likes[lastPost]--;
-
-            Console.WriteLine("Undo successful! Removed like from " + lastPost);
-        }
-        else
-        {
-            Console.WriteLine("No actions to undo!");
-        }
-    }
-
-    static void ShowPosts()
-    {
-        Console.WriteLine("\nPosts & Likes:");
-        foreach (var post in likes)
-        {
-            Console.WriteLine(post.Key + " -> " + post.Value + " likes");
-        }
-    }
-
-    static void ProcessNotifications()
-    {
-        Console.WriteLine("\nNotifications:");
-        while (notifications.Count > 0)
-        {
-            Console.WriteLine(notifications.Dequeue());
-        }
+        MusicPlaylistManager manager = new MusicPlaylistManager();
+        manager.Addsong("Song1", 5, "Artist1");
+        manager.Addsong("Song2", 4, "Artist2");
+        manager.Addsong("Song3", 3, "Artist3");
+        manager.DisplayPlaylist();
+        manager.DisplaySongsByRating();
+        manager.DisplaySongsByGenre();
+        manager.RemoveSong("Song2");
+        manager.DisplayPlaylist();
+        manager.DisplaySongsByRating();
+        manager.DisplaySongsByGenre();
     }
 }
