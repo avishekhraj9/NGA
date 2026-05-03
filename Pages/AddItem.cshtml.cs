8f1cc988d6f03d9b607ef1132275e8051b26c6cd
AVISHEKH RAJ <75256697+avishekhraj9@users.noreply.github.com>
2026-05-03 18:31:25 +0530

    Day10 Practical Assignment 3 P2 RazorApp

---
+++ Pages/AddItem.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class AddItemModel : PageModel
+{
+    [BindProperty]
+    public string NewItem { get; set; }
+
+    public string Message { get; set; }
+
+    public void OnPost()
+    {
+        Message = "Item Added: " + NewItem;
+    }
+}
---
+++ Pages/Items.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+public class ItemsModel : PageModel
+{
+    public List<string> Items { get; set; }
+
+    public void OnGet()
+    {
+        Items = new List<string> { "Apple", "Banana", "Mango" };
+    }
+}
